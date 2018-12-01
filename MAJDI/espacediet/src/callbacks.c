1925a00b2d01e43631fbc79fdf650df33da7c498
majdilahsini <lahsini.majdi@gmail.com>
2018-12-01 10:41:32 -0800

    design

---
+++ MAJDI/espacediet/src/callbacks.c
@@ -0,0 +1,66 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_accuei_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_profil_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_fchdiet_diet_clicked                (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_fichemed_diet_clicked               (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_rendez_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_dec_diet_clicked                    (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_quit_diet_clicked                   (GtkToolButton   *toolbutton,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ MAJDI/espacediet/src/callbacks.h
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+
+
+void
+on_accuei_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_profil_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_fchdiet_diet_clicked                (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_fichemed_diet_clicked               (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_rendez_diet_clicked                 (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_dec_diet_clicked                    (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
+
+void
+on_quit_diet_clicked                   (GtkToolButton   *toolbutton,
+                                        gpointer         user_data);
